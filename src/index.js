471fe49572d07b32dfd206fb191e8b72f8825d2a
oopanpan <oopanpan@gmail.com>
2021-02-23 17:03:43 -0800

    Pan Syd Erik

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form')
+  const ul = document.querySelector('ul#tasks')
+  //grab that shit
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const text = form.querySelector('input');
+    const li = document.createElement('li');
+    const delButton = document.createElement('button');
+    delButton.classList.add('button')
+    delButton.innerText = 'X'
+    ul.appendChild(li);
+    li.innerText = text.value + " ";
+    li.appendChild(delButton);
+  })
+  ul.addEventListener('click', function(e){
+    if (e.target.classList.value === 'button'){
+      e.target.parentElement.remove()
+    }
+    else if (e.target.tagName.toUpperCase() === 'LI'){
+      alert(e.target.innerText)
+    }
+  })
 });
+
